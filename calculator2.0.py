b0fa8102212e3906cf83aa59564798edd41b07c5
apiimmars <139563440+apiimmars@users.noreply.github.com>
2023-07-14 20:56:06 +0700

    Add files via upload
    iseng aja

---
+++ calculator2.0.py
@@ -0,0 +1,56 @@
+#calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: math error"
+
+#f(main.calc)
+def calculator():
+    print("Simple Calculator")
+    print("-----------------")
+
+    while True:
+        print("Choose operation:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == '5':
+            print("Goodbye!")
+            break
+
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+        else:
+            print("Invalid choice!")
+            continue
+
+        print("Result: ", result)
+        print("-----------------")
+
+#run
+calculator()
